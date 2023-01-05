ae53e388785bec89bbcbfa7d47e0e32e76a2beab
Marat <brutto061@gmail.com>
2023-01-05 16:01:27 +0700

    выполили задание №5

---
+++ zadanie_5.py
@@ -0,0 +1,16 @@
+#Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
+
+
+def fib(n):
+    if n <= 1:
+        return n
+    else:
+        return fib(n-1) + fib(n-2)
+
+index = int(input('Введите коэффициент: ')) 
+my_list = []
+for i in range(index+1):
+    my_list.append(fib(i))
+    if i > 0:
+        my_list.insert(0, fib(i)*pow(-1, i+1))
+print(*my_list, sep=', ')
